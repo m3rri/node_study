2fdb9dfea785ddaeaefb49de0119486c68cc5ca4
m3rri <m3rri17@gmail.com>
2021-06-21 23:56:45 +0900

    zerocho - making server-2

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const http = require('http');
 http.createServer((req, res)=>{
-    console.log('server start');
+    return req
+    .on('error', err=>{
+        console.error(err);
+    })
+    .on('data', data=>{
+        console.log(data);
+    })
+    .on('end', ()=>{
+        res.on('error', err=>{
+            console.error(err);
+        });
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.write('hi~\n');
+        res.end('the end');
+    });
 }).listen(8080);
