e961741d43d9179d2592445149f21fb19fbcb59e
m3rri <m3rri17@gmail.com>
2021-06-22 23:57:43 +0900

    zerocho - express > middleware & routing

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const exp = require('express');
 const path = require('path');
 const app = exp();
+const route = require('./route.js');
 
 app.use('/pseudo', exp.static(path.join(__dirname, 'html')));
+app.use((req, res, next)=>{
+    console.info('request url :: '+req.url);
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'html', 'main.html'));
+    if(req.url.indexOf('bomb')>=0){
+        console.info('error'+error);
+    }else{
+        next();
+    }
+});
+app.use((req, res, next)=>{
+    res.status(404).send('not found url');
+});
+app.use((err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500).send('server error');
 });
 
-app.get('/about', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'html', 'about.html'));
-});
+app.use('/', route);
 
 app.listen(8080, ()=>{
     console.log('Express App on port 8080');
